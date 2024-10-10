e1b752cfb09f13355ceb4872b809935d96456c64
anda elaaser <andaelaaser@gmail.com>
2024-10-10 19:36:28 +0300

    fixed

--- src/app/Interfaces/iproduct.ts
+++ src/app/Interfaces/iproduct.ts
@@ -1,5 +1,4 @@
 export interface IProduct {
-<<<<<<< HEAD
   image: string,
   name: string,
   oldPrice: string,
@@ -8,14 +7,4 @@ export interface IProduct {
   scale: number,
   count?: number,
   Type: 'Decoration' | 'Accessory' | 'Furniture',
-=======
-  image: string;
-  name: string;
-  oldPrice: string;
-  newPrice: string;
-  isoffered: boolean;
-  scale: number;
-  count?: number;
-  Type: 'Decoration' | 'Accessory' | 'Furniture';
->>>>>>> e1471eeee553a2c849e9b32530b0baf84b3bab35
 }
