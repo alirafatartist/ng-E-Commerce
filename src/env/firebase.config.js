67f317ecb9d592c7a4b252efe4a8aa8f4edfe450
alirafatartist <lraft636@gmail.com>
2024-10-15 17:44:58 +0300

    style nav

---
+++ src/env/firebase.config.js
@@ -0,0 +1,20 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
+  import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
+
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDoEoHtrMHikzvFHaw-j3b3Xf4Jd-dmMsQ",
+    authDomain: "ng-e-commerce-f3ffb.firebaseapp.com",
+    projectId: "ng-e-commerce-f3ffb",
+    storageBucket: "ng-e-commerce-f3ffb.appspot.com",
+    messagingSenderId: "642342689892",
+    appId: "1:642342689892:web:22d871c9be583250821371"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
